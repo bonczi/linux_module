189c9684fc36aa9eb516e8fabd675fb7c8a9b753
Piotr Bąk <pbonczi7@gmail.com>
2020-12-03 15:32:32 +0100

    Developing timestamp

--- new2.c
+++ new2.c
@@ -7,7 +7,7 @@
 #include <linux/uaccess.h>
 #include <linux/sched.h>
 #include <linux/workqueue.h>
-#include <linux/interrupt.h>	
+#include <linux/interrupt.h>
 #include <linux/fs.h>
 #include <linux/buffer_head.h>
 #include <linux/string.h>
@@ -24,9 +24,13 @@ static int counter_key = 0;
 module_param(counter_key,int, 0660);
 
 static char my_timestamp[BUFSIZE];
+static char * daystr = "Mon";
+static char * monstr = "Jan";
   
 static ssize_t myread(struct file *file, char __user *ubuf,size_t count, loff_t *ppos);
 static ssize_t mywrite(struct file *file, const char __user *ubuf,size_t count, loff_t *ppos);
+static int convert_dayweek(int day, char ** str);
+static int convert_month(int day, char ** str);
 
 static void do_gettimeofday(struct timeval *tv)
 {
@@ -40,55 +44,61 @@ static struct proc_dir_entry *ent;
  
 static struct file_operations myops =
 {
-	.owner = THIS_MODULE,
-	.read = myread,
-	.write = mywrite,
+    .owner = THIS_MODULE,
+    .read = myread,
+    .write = mywrite,
 };
 
 static ssize_t myread(struct file *file, char __user *ubuf,size_t count, loff_t *ppos) 
 {
-	char buf[BUFSIZE];
-	int len=0;
-	
-	if(*ppos > 0 || count < BUFSIZE)
-		return 0;
-		
-	len += sprintf(buf + len,"counter = %d\n", counter_key);
-	len += sprintf(buf + len,"%s", my_timestamp);
-	
-	if(copy_to_user(ubuf,buf,len))
-		return -EFAULT;
-		
-	*ppos = len;
-	
-	return len;
+    char buf[BUFSIZE];
+    int len=0;
+
+    if(*ppos > 0 || count < BUFSIZE)
+        return 0;
+
+    len += sprintf(buf + len,"counter = %d\n", counter_key);
+    len += sprintf(buf + len,"%s", my_timestamp);
+
+    if(copy_to_user(ubuf,buf,len))
+        return -EFAULT;
+
+    *ppos = len;
+
+    return len;
 }
 
 static ssize_t mywrite(struct file *file, const char __user *ubuf,size_t count, loff_t *ppos) 
 {
-	int c;
-	char buf[BUFSIZE];
-	struct timeval t;
-	struct tm broken;
-	
-	if(*ppos > 0 || count > BUFSIZE)
-		return -EFAULT;
-	if(copy_from_user(buf,ubuf,count))
-		return -EFAULT;
+    int c;
+    char buf[BUFSIZE];
+    struct timeval t;
+    struct tm broken;
 
-	if (strcmp(buf,"Set zero") == 0u)
-	{
-		counter_key = 0;
+    if(*ppos > 0 || count > BUFSIZE)
+        return -EFAULT;
+    if(copy_from_user(buf,ubuf,count))
+        return -EFAULT;
 
-		do_gettimeofday(&t);
-		time64_to_tm(t.tv_sec, 0, &broken);
-		sprintf(my_timestamp,"%02d:%02d:%02d:%ld\n", broken.tm_hour, broken.tm_min,
-								 broken.tm_sec, t.tv_usec);
-	}
+    if (strcmp(buf,"Set zero") == 0u)
+    {
+        counter_key = 0;
 
-	*ppos = strlen(buf);
+        do_gettimeofday(&t);
+        time64_to_tm(t.tv_sec, 0, &broken);
 
-	return c;
+        if (convert_dayweek(broken.tm_wday, &daystr) && convert_month(broken.tm_mon, &monstr))
+        {
+        	sprintf(my_timestamp,"Last file modification:   %s %s  %d %02d:%02d:%02d %ld\n",
+        			daystr, monstr, broken.tm_mday, broken.tm_hour+1, broken.tm_min, broken.tm_sec, broken.tm_year+1900);
+        }
+
+
+    }
+
+    *ppos = strlen(buf);
+
+    return c;
 }
  
  
@@ -101,26 +111,126 @@ static irqreturn_t kbd3_isr(int irq, void *dev_id)
  
 static int simple_init(void)
 {
-	struct timeval t;
-	struct tm broken;
+    struct timeval t;
+    struct tm broken;
 
-	ent=proc_create("mydev",0660,NULL,&myops);
-	printk(KERN_ALERT "hello...\n");
+    ent=proc_create("mydev",0660,NULL,&myops);
+    printk(KERN_ALERT "hello...\n");
 
-	do_gettimeofday(&t);
-	time64_to_tm(t.tv_sec, 0, &broken);
-	sprintf(my_timestamp,"%02d:%02d:%02d:%ld\n", broken.tm_hour, broken.tm_min, broken.tm_sec, t.tv_usec);
+    do_gettimeofday(&t);
+    time64_to_tm(t.tv_sec, 0, &broken);
 
-	return request_irq(KBD_IRQ, kbd3_isr, IRQF_SHARED, "kbd3", (void *)kbd3_isr);
+    if (convert_dayweek(broken.tm_wday, &daystr) && convert_month(broken.tm_mon, &monstr))
+    {
+    	sprintf(my_timestamp,"Last file modification:   %s %s  %d %02d:%02d:%02d %ld\n",
+    			daystr, monstr, broken.tm_mday, broken.tm_hour+1, broken.tm_min, broken.tm_sec, broken.tm_year+1900);
+    }
+
+    return request_irq(KBD_IRQ, kbd3_isr, IRQF_SHARED, "kbd3", (void *)kbd3_isr);
 }
  
 static void simple_cleanup(void)
 {
     printk(KERN_WARNING "bye ...\n");
-	proc_remove(ent);
-	free_irq(KBD_IRQ, (void *)kbd3_isr);
+    proc_remove(ent);
+    free_irq(KBD_IRQ, (void *)kbd3_isr);
 }
  
+
+
+static int convert_dayweek(int day, char ** str)
+{
+    int success = 0u;
+
+	if ( (str != NULL) && (day >= 0) && (day < 7) )
+	{
+		switch (day)
+		{
+			case (0u):
+					*str = "Sun";
+					break;
+			case (1u):
+					*str = "Mon";
+					break;
+			case (2u):
+					*str = "Tue";
+					break;
+			case (3u):
+					*str = "Wed";
+					break;
+			case (4u):
+					*str = "Thu";
+					break;
+			case (5u):
+					*str = "Fri";
+					break;
+			case (6u):
+					*str = "Sat";
+					break;
+			default:
+				break;
+		}
+		success = 1u;
+	}
+
+    return success;
+}
+
+
+
+static int convert_month(int mon, char ** str)
+{
+    int success = 0u;
+
+	if ( (str != NULL) && (mon >= 0) && (mon < 12) )
+	{
+		switch (mon)
+		{
+			case (0u):
+					*str = "Jan";
+					break;
+			case (1u):
+					*str = "Feb";
+					break;
+			case (2u):
+					*str = "Mar";
+					break;
+			case (3u):
+					*str = "Apr";
+					break;
+			case (4u):
+					*str = "May";
+					break;
+			case (5u):
+					*str = "Jun";
+					break;
+			case (6u):
+					*str = "Jul";
+					break;
+			case (7u):
+					*str = "Aug";
+					break;
+			case (8u):
+					*str = "Sep";
+					break;
+			case (9u):
+					*str = "Oct";
+					break;
+			case (10u):
+					*str = "Nov";
+					break;
+			case (11u):
+					*str = "Dec";
+					break;
+			default:
+				break;
+		}
+		success = 1u;
+	}
+
+    return success;
+}
+
 module_init(simple_init);
 module_exit(simple_cleanup);
 
